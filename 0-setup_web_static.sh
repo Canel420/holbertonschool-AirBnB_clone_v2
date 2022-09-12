9d11702574e0de3743e1b9191ea7ed611850acab
Canel420 <4521@holbertonstudents.com>
2022-09-11 20:17:18 -0500

    don't know what to do anymore

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Script that sets up your web servers for the deployment of web_static
 
-sudo apt update
+sudo apt -y update
 sudo apt -y install nginx
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
@@ -17,4 +17,3 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i "/server_name _;/ a \n\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
 sudo service nginx restart
-
