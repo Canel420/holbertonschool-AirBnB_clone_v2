76ab9a2875ce3e02b15574ff64673777a2a5507a
Canel420 <4521@holbertonstudents.com>
2022-09-14 12:10:40 -0500

    back to past stage

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,21 +4,22 @@
 Script that starts a Flask web application.
 """
 from flask import Flask, render_template
-import models
+from models import storage
+from models.state import State
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
 def teardown_db(self):
     """ closes or deallocates the resource if exists"""
-    models.storage.close()
+    storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def all_cities():
     """ Displays a HTML page """
     return render_template('7-states_list.html',
-                           states=models.storage.all('State'))
+                           states=storage.all(State))
 
 
 if __name__ == '__main__':
--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -4,28 +4,29 @@
 Script that starts a Flask web application.
 """
 from flask import Flask, render_template
-import models
+from models import storage
+from models.state import State
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
 def teardown_db(self):
     """ closes or deallocates the resource if exists"""
-    models.storage.close()
+    storage.close()
 
 
 @app.route('/states', strict_slashes=False)
 def all_states():
     """ Display a HTML page with states """
     return render_template('7-states_list.html',
-                           states=models.storage.all('State'))
+                           states=storage.all(State))
 
 
 @app.route('/states/<string:id>', strict_slashes=False)
 def search_state(id=None):
     """ Displays a Html page with a state search """
     return render_template('9-state.html',
-                           states=models.storage.all('State')
+                           states=storage.all(State)
                            .get('State.{}'.format(id)))
 
 
